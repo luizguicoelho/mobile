6ec1652a2aa8c5a40064f94ba6f64a6ec51031cf
luizguicoelho <163432443+luizguicoelho@users.noreply.github.com>
2025-04-09 11:22:49 -0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,84 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image"
+import { Ionicons } from "@expo/vector-icons"
+
+// npm install expo-image (usar este código no terminal)
+
+const foto = require("../assets/images/paisagem.jpg")
+
+export default function Index() {
+  return (
+    <View style={styles.container} >
+      <View style = {styles.containerImg}>
+        <Image source = {foto} style={styles.estiloFoto}></Image>
+      </View>
+      <View style = {styles.containerConteudo}>
+        <View style = {styles.containerNome}>
+          <Text style={styles.nome}>Luiz Guilherme Coelho</Text>
+        </View>
+        <Text style = {styles.linha}>
+          ____________________________________________
+        </Text>
+        <View style = {styles.containerDados}>
+          <Ionicons name="person" size={24} color="white" />
+          <Text style = {styles.textoDados}>16 anos</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="mail" size={24} color="white" />
+          <Text style = {styles.textoDados}>Luiz.Guilherme.Coelho@escola.pr.gov.br</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="call" size={24} color="white" />
+          <Text style = {styles.textoDados}>(42) 995649-0266</Text>
+        </View>
+        <View style = {styles.containerDados}>
+          <Ionicons name="home" size={24} color="white" />
+          <Text style = {styles.textoDados}>Ponta Grossa / PR</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  containerImg: {
+    flex: 1,
+    paddingTop: 60,
+  },
+  estiloFoto: {
+    width: 600,
+    height: 350,
+  },
+  containerConteudo: {
+    flex: 1
+  },
+  containerNome:{
+    alignItems:"center",
+  },
+  nome: {
+    fontSize: 40,
+    color: "white",
+    fontWeight: "bold",
+  },
+  linha:{
+    color: "white",
+    fontSize: 20,
+    marginBottom: 20
+  },
+  containerDados: {
+    marginBottom: 8,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  textoDados: {
+    marginLeft: 10,
+    color: "white",
+    fontSize: 24,
+  }
+})
